575000b25be474e6a72e785fdd9f92ec0ec8d04e
kyky19 <kyky19@me.com>
2022-10-18 21:28:41 -0500

    update

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtbyId,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getAllThoughts);
+router
+  .route("/:id")
+  .get(getThoughtbyId)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route("/:userId").post(createThought);
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
